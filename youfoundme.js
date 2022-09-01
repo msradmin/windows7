4b4ead7c2a395c46b6aa87c489442ce92b97351c
msradmin <86022985+msradmin@users.noreply.github.com>
2022-09-01 10:25:18 -0300

    i don't know what i type.

---
+++ youfoundme.js
@@ -0,0 +1,6 @@
+new explorer.window()
+.title('H')
+.resize(1280, 720)
+.callback(function() {
+    this.body.html('wow, you found me! thanks for using my simulator! this is an simulator thats recreates Windows 7 RTM Version, powered with Below Average (c) Webdows source code.');
+});
