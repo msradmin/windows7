5f80c6da9d0d7a66a77a4562a4919d762958ed92
msroffice <86022985+msradmin@users.noreply.github.com>
2023-11-14 11:47:21 -0300

    Update youfoundme.js

--- youfoundme.js
+++ youfoundme.js
@@ -1,6 +1,15 @@
 new explorer.window()
-.title('Testing Edge Browser API')
-.resize(320, 200)
-.callback(function() {
-    this.body.html('ADD STUFF HERE');
-});
+    .title('Google Chrome')
+    .resize(800, 600)
+    .callback(function() {
+        // Cria um iframe para exibir o conteúdo da página da web com o parâmetro igu=1
+        var iframe = document.createElement('iframe');
+        iframe.style.width = '100%';
+        iframe.style.height = '100%';
+        iframe.style.border = 'none';
+        iframe.src = 'https://www.google.com?igu=1'; // URL com o parâmetro igu=1
+
+        // Adiciona o iframe ao corpo da janela
+        this.body.appendChild(iframe);
+    });
+
